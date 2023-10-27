b31125e96a57273814dbfb1650d71e5ba9c709a9
Ahmed H Elmi <68184104+mayamaya99@users.noreply.github.com>
2023-10-27 15:09:20 -0700

    Connect to MongoDb
    Able to connect to Mongodb successfully.

---
+++ index.js
@@ -0,0 +1,17 @@
+
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+mongoose.connect('mongodb+srv://elmiUser:Amina@cluster0.fh0dilb.mongodb.net/MarketPlace?retryWrites=true&w=majority').then(() => {
+    console.log('Connected to Mongodb');
+}).catch((err) => {
+    console.log(err)
+})
+const app = express();
+app.listen(3000, () => {
+    console.log("Server is running !!!");
+});
+
