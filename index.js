69c96984dc31dde896bf1231d0da78ed9e43182b
Ahmed H Elmi <68184104+mayamaya99@users.noreply.github.com>
2023-10-28 10:25:49 -0700

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import mongoose from "mongoose";
 import dotenv from 'dotenv';
 
 dotenv.config();
-
+//mongoose.connect(process.env.MONGO)
 mongoose.connect('mongodb+srv://elmiUser:Amina@cluster0.fh0dilb.mongodb.net/Ecommerce?retryWrites=true&w=majority').then(() => {
     console.log('Connected to Mongodb');
 }).catch((err) => {
